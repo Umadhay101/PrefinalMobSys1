743cd8deba0f43488fb9ced7de0efba9242e38a9
khronosangel <khronosangel@gmail.com>
2025-03-21 12:49:58 +0800

    Add project files.

---
+++ PrefinalMobSys1/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PrefinalMobSys1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PrefinalMobSys1/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PrefinalMobSys1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
